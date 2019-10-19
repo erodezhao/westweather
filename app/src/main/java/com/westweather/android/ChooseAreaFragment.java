37b2941092384e0b097a4c5b245d95fd4ca58c68
erodezhao <skyhp747@gmail.com>
2019-10-19 23:54:46 +0800

    遍历全国省市县数据。

---
+++ app/src/main/java/com/westweather/android/ChooseAreaFragment.java
@@ -0,0 +1,56 @@
+package com.westweather.android;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.westweather.android.db.City;
+import com.westweather.android.db.County;
+import com.westweather.android.db.Province;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChooseAreaFragment extends Fragment {
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listview;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList = new ArrayList<>();
+
+    //省列表
+    private List<Province> provinceList;
+    //市列表
+    private List<City> cityList;
+    //县列表
+    private List<County> countyList;
+    //选中的省份
+    private Province selectedProvince;
+    //选中的城市
+    private City selectedCity;
+    //选中的级别
+    private int currentLevel;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.choose_area, container, false);
+        titleText = (TextView)view.findViewById(R.id.title_text);
+        backButton = (Button)view.findViewById(R.id.back_button);
+        listview = (ListView)view.findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, dataList);
+        listview.setAdapter(adapter);
+        return view;
+    }
+
+}
